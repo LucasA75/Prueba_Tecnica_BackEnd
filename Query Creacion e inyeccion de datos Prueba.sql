6120cb2e6fc27ba42eb6795173e99a17cd1210f0
LucasA75 <lucasacevedocastro@gmail.com>
2023-04-06 10:05:24 -0400

    Query de MySQL

---
+++ Query Creacion e inyeccion de datos Prueba.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE IF NOT EXISTS prueba_tecnica;
+
+-- Datos de supervisores
+INSERT INTO prueba_tecnica.supervisor(id, birth_date, email, last_name, name, phone) values
+(1,"1999-1-3","Camilo@gmail.com","Montoya","Camilo",958331142),
+(2,"1985-12-30","Manuel@gmail.com","Castro","Manuel",952454342),
+(3,"1993-5-8","Pedro@gmail.com","Urrutia","Pedro",958249642);
+
+-- Datos de empleados  
+INSERT INTO prueba_tecnica.empleado(id, birth_date, email, last_name, name, phone, id_supervisor) values
+(1,"1999-1-3","Camilo@gmail.com","Montoya","Camilo",958331142,1),
+(2,"1985-12-30","Manuel@gmail.com","Castro","Manuel",952454342,2),
+(3,"1993-5-8","Pedro@gmail.com","Urrutia","Pedro",958249642,3);
+
+-- Los demas empleados los agregare por la API
+
+
+SELECT * FROM prueba_tecnica.supervisor;
+SELECT * FROM prueba_tecnica.empleado;
+
+
