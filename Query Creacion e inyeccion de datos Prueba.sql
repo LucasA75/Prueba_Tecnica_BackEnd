3d0d2e18cd4a75caccec3ea9621655ccaa6b0c60
LucasA75 <lucasacevedocastro@gmail.com>
2023-04-06 12:39:20 -0400

    ultimos cambios

--- Query Creacion e inyeccion de datos Prueba.sql
+++ Query Creacion e inyeccion de datos Prueba.sql
@@ -8,14 +8,12 @@ INSERT INTO prueba_tecnica.supervisor(id, birth_date, email, last_name, name, ph
 
 -- Datos de empleados  
 INSERT INTO prueba_tecnica.empleado(id, birth_date, email, last_name, name, phone, id_supervisor) values
-(1,"1999-1-3","Camilo@gmail.com","Montoya","Camilo",958331142,1),
-(2,"1985-12-30","Manuel@gmail.com","Castro","Manuel",952454342,2),
-(3,"1993-5-8","Pedro@gmail.com","Urrutia","Pedro",958249642,3);
+(1,"1999-1-3","Camilo@gmail.com","Montoya","Camilo",958331142,null),
+(2,"1985-12-30","Manuel@gmail.com","Castro","Manuel",952454342,null),
+(3,"1993-5-8","Pedro@gmail.com","Urrutia","Pedro",958249642,null);
 
 -- Los demas empleados los agregare por la API
 
 
 SELECT * FROM prueba_tecnica.supervisor;
 SELECT * FROM prueba_tecnica.empleado;
-
-
